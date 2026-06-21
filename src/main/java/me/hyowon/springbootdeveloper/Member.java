2ed5225c3b5c21a1c91cc700f852aba7c46a3187
NotHyooon <gydnjs3505@gmail.com>
2026-06-22 00:31:30 +0900

    feat : chapter 5 데이터베이스 조작이 편해지는 ORM 실습

--- src/main/java/me/hyowon/springbootdeveloper/Member.java
+++ src/main/java/me/hyowon/springbootdeveloper/Member.java
@@ -9,13 +9,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 생성, 접근 제어자 protected
 @AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자 생성
 @Getter
-@Entity
+@Entity // 엔티티로 지정
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 생성 전략
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 생성 전략, 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id; // DB 테이블의 'id' 컬럼과 매칭
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false)    // name이라는 not null 컬럼과 매핑
     private String name; // DB 테이블의 'name' 컬럼과 매칭
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
